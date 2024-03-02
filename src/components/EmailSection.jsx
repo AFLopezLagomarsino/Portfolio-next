d36306f46e579ffcaf0b523b773332d8490ca3b4
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2024-03-01 21:12:18 -0300

    componente EmailSection terminado

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -1,9 +1,49 @@
+"use client";
+import { useState } from "react";
+import {ToastContainer, toast, Zoom} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
 import GithubIcon from "../../public/github.svg";
 import LinkedinIcon from "../../public/linkedin.svg";
 import Link from "next/link";
 import Image from "next/image";
+import { RocketLaunchIcon } from "@heroicons/react/24/outline";
 
 const EmailSection = () => {
+
+  const notify = () => toast.success("Email enviado!",{
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Zoom,
+  });
+
+  const [input, setInput] = useState({
+    email:"",
+    subject:"",
+    message:""
+  })
+
+  function handleChange(e){
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e){
+    setInput({
+      email:"",
+      subject:"",
+      message:""
+    })
+  }
+
+
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-red-600 to-tranparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2 ">
@@ -17,16 +57,16 @@ const EmailSection = () => {
           contactarme y te repondere apenas pueda!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/AFLopezLagomarsino">
+          <Link   target="_blank" href="https://github.com/AFLopezLagomarsino">
             <Image src={GithubIcon} alt="github" width={50} />
           </Link>
-          <Link href="https://www.linkedin.com/in/andres-lopez-lagomarsino/">
+          <Link target="_blank" href="https://www.linkedin.com/in/andres-lopez-lagomarsino/">
             <Image src={LinkedinIcon} alt="linkedin" width={50} />
           </Link>
         </div>
       </div>
       <div>
-        <form className="flex flex-col">
+        <form className="flex flex-col" action="https://formsubmit.co/andresinfernoxii@gmail.com" method="POST" onSubmit={e=>handleSubmit(e)}>
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -36,9 +76,11 @@ const EmailSection = () => {
             </label>
             <input
               type="email"
+              name="email"
               id="email"
               required
               placeholder="Email"
+              onChange={e=>handleChange(e)}
               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
             />
           </div>
@@ -52,8 +94,10 @@ const EmailSection = () => {
             <input
               type="text"
               id="subject"
+              name="subject"
               required
               placeholder="escribe tu asunto aqui :D"
+              onChange={e=>handleChange(e)}
               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
             />
           </div>
@@ -67,17 +111,23 @@ const EmailSection = () => {
             <textarea
               name="message"
               id="message"
-              placeholder="escribee tu mensaje aqui :V"
+              placeholder="escribe tu mensaje aqui :V"
+              onChange={e=>handleChange(e)}
               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
             />
           </div>
           <button
+            onClick={notify}
             type="submit"
             className="bg-red-800 hover:bg-red-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
           >
             Enviar email
           </button>
+          <input type="hidden" name="_next" value="http://localhost:3000" />
+          <input type="hidden" name="_captcha" value="false" />
         </form>
+        <ToastContainer
+        icon={<RocketLaunchIcon/>}/>
       </div>
     </section>
   );
