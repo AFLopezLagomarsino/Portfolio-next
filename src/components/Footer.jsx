e194feba635b485f021b29887f499806dfbfc9ca
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2024-03-01 21:12:40 -0300

    componente Footer terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image"
+import Logo from "../../public/LogoXII.png"
 
 const Footer = () => {
   return (
-    <footer className="footer border border-t-[#141414] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-500 text-sm">All rights reserved</p>
+    <footer className="footer border z-10 border-t-[#141414] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-center items-center lg:ml-40">
+        <Image src={Logo} alt="Logo" width={100} height={100}/>
       </div>
     </footer>
   )
